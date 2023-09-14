c51a31395f758a17b10674c695f8554a6f1dc6a2
Diah Putri Nofianti <diahputrinofianti@gmail.com>
2023-09-14 09:34:04 +0700

    first commit

---
+++ bin/dart_application.dart
@@ -0,0 +1,69 @@
+import 'package:dart_application/dart_application.dart' as dart_application;
+
+void main() {
+  // print('Hello world: ${dart_application.calculate()}!');
+
+  dynamic data = 'Hello World';
+  data = 'Sedang mencoba dynamic di dart...\n';
+  print(data);
+
+/*
+  var a = 'Hello World';
+  a = 21; // Error
+  print(a);
+*/
+
+//Fungsi
+
+  int angka = 81;
+
+  void predikat(nilai) {
+    if (nilai >= 81 && nilai <= 100) {
+      print('Nilai A');
+    } else if (nilai >= 76 && nilai <= 80) {
+      print('Nilai B');
+    } else if (nilai >= 71 && nilai <= 75) {
+      print('Nilai C');
+    } else if (nilai >= 66 && nilai <= 70) {
+      print('Nilai D');
+    } else if (nilai < 66) {
+      print('Nilai E');
+    }
+    print('\n');
+  }
+
+  predikat(angka);
+
+// Perulangan
+
+  var smile = '';
+  for (var i = 0; i < 5; i++) {
+    // increment sampai 5 baris
+    for (var j = 0; j <= i; j++) {
+      // increment untuk menambahkan smile di setiap baris
+      smile += '• ';
+    }
+    smile += '\n';
+  }
+  for (var i = 4; i >= 0; i--) {
+    // decrement dari 4 sampai 0 baris
+    for (var j = 1; j <= i; j++) {
+      smile += '• ';
+    }
+    smile += '\n';
+  }
+  print(smile);
+
+// Perulangan lain
+
+  var senyum = '😊';
+  for (var i = 0; i <= 5; i++) {
+    print(senyum * i);
+  }
+
+  for (var j = 4; j >= 0; j--) {
+    print(senyum * j);
+  }
+}
+
+/// https://www.anbidev.com/dart-pengenalan
