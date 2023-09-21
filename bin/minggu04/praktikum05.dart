9c0e475a4cbc6c9cfb64fc890673d541f4068981
Diah Putri Nofianti <diahputrinofianti@gmail.com>
2023-09-21 17:54:37 +0700

    praktikum05

--- bin/minggu04/praktikum05.dart
+++ bin/minggu04/praktikum05.dart
@@ -1,23 +1,30 @@
-void main() {
-  // langkah 1
-  var record = ('first', a: 2, b: true, 'last');
-  print(record);
-
-  // langkah 4
-  (String, int) mahasiswa;
-  // print(mahasiswa);
-
-  // langkah 5
-  var mahasiswa2 = ('first', a: 2, b: true, 'last');
-
-  print(mahasiswa2.$1); // Prints 'first'
-  print(mahasiswa2.a); // Prints 2
-  print(mahasiswa2.b); // Prints true
-  print(mahasiswa2.$2); // Prints 'last'
-}
-
-// langkah 3
+// Langkah 3
 (int, int) tukar((int, int) record) {
   var (a, b) = record;
   return (b, a);
+}
+
+void main() {
+  // Langkah 1
+  var record = ('first', a: 2, b: true, 'last');
+  print(record);
+
+  var record1 = (1, 2);
+  print(record1);
+
+  var nilai = tukar(record1);
+  print(nilai);
+
+  // Langkah 4
+  (String, int) mahasiswa;
+  mahasiswa = ('Diah Putri Nofianti', 2141720054);
+  print(mahasiswa);
+
+  // Langkah 5
+  var mahasiswa2 = ('Diah Putri Nofianti', a: '2141720054', b: true, 'last');
+
+  print(mahasiswa2.$1);
+  print(mahasiswa2.a);
+  print(mahasiswa2.b);
+  print(mahasiswa2.$2);
 }
