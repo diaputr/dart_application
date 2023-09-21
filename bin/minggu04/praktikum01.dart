e94ff9b8b92b4f41507ca9845852bab1f7876047
Diah Putri Nofianti <diahputrinofianti@gmail.com>
2023-09-21 09:42:05 +0700

    message: manage file

---
+++ bin/minggu04/praktikum01.dart
@@ -0,0 +1,13 @@
+import 'package:dart_application/dart_application.dart' as dart_application;
+
+void main() {
+  var list = [1, 2, 3];
+  assert(list.length == 3);
+  assert(list[1] == 2);
+  print(list.length);
+  print(list[1]);
+
+  list[1] = 1;
+  assert(list[1] == 1);
+  print(list[1]);
+}
